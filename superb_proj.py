74bf948d6d1ca98107a1127fd6ed2f085df1fde9
Amina Isah <103606544+Superb25@users.noreply.github.com>
2022-09-29 15:58:34 -0700

    Update superb_proj.py

--- superb_proj.py
+++ superb_proj.py
@@ -62,10 +62,10 @@ st.line_chart(chart_data)
 st.sidebar.write('Code on Github repository')
 st.sidebar.write('https://github.com/Superb25/Amina_ADS_proj/blob/main/ADS%20PROJECT.ipynb')
 
-import pickle
-filename = 'trained_model.sav'
-pickle.dump(model, open(filename, 'wb'))
-load_model = pickle.load(open('trained_model.sav', 'rb'))
+#import pickle
+#filename = 'trained_model.sav'
+#pickle.dump(model, open(filename, 'wb'))
+#load_model = pickle.load(open('trained_model.sav', 'rb'))
 
 
 # creating a function for prediction
